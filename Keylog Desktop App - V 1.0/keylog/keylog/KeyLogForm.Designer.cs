e47725d72fd486883122c0f8e29960bfa0dc1292
DantonAlexander <57826961+DantonAlexander@users.noreply.github.com>
2019-11-16 05:23:06 -0500

    Add files via upload
    V1.0 - Only mouse movements are recorded.

---
+++ Keylog Desktop App - V 1.0/keylog/keylog/KeyLogForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace keylog
+{
+    partial class KeyLogForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KeyLogForm));
+            this.btnRec = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPanel = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnX = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnY = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.btnPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnRec
+            // 
+            this.btnRec.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(140)))), ((int)(((byte)(255)))));
+            this.btnRec.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRec.Location = new System.Drawing.Point(18, 11);
+            this.btnRec.Name = "btnRec";
+            this.btnRec.Size = new System.Drawing.Size(120, 64);
+            this.btnRec.TabIndex = 0;
+            this.btnRec.Text = "RECORD";
+            this.btnRec.UseVisualStyleBackColor = false;
+            this.btnRec.Click += new System.EventHandler(this.btnRec_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(204)))), ((int)(((byte)(102)))));
+            this.btnPlay.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(159, 11);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(120, 64);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "PLAY";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.btnStop.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(297, 11);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(120, 64);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPanel
+            // 
+            this.btnPanel.Controls.Add(this.btnStop);
+            this.btnPanel.Controls.Add(this.btnPlay);
+            this.btnPanel.Controls.Add(this.btnRec);
+            this.btnPanel.Location = new System.Drawing.Point(6, 15);
+            this.btnPanel.Name = "btnPanel";
+            this.btnPanel.Size = new System.Drawing.Size(431, 92);
+            this.btnPanel.TabIndex = 3;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnX,
+            this.columnY});
+            this.listView1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.Location = new System.Drawing.Point(24, 113);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(399, 325);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnX
+            // 
+            this.columnX.Text = "X";
+            // 
+            // columnY
+            // 
+            this.columnY.Text = "Y";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // KeyLogForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(451, 107);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnPanel);
+            this.Font = new System.Drawing.Font("Colonna MT", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "KeyLogForm";
+            this.Text = "KeyLog - V 1.0";
+            this.Load += new System.EventHandler(this.KeyLogForm_Load);
+            this.btnPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRec;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Panel btnPanel;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ColumnHeader columnX;
+        private System.Windows.Forms.ColumnHeader columnY;
+    }
+}
+
---
+++ Keylog Desktop App - V 1.0/keylog/keylog/KeyLogForm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+///
+/// Code by - Danton Alexander
+/// 
+/// Version 1.0 - June 2019
+/// 
+
+namespace keylog
+{
+    public partial class KeyLogForm : Form
+    {
+        public KeyLogForm()
+        {
+            InitializeComponent();
+        }
+
+
+        //// Variables ////
+
+        ListViewItem lvEventLog;
+        int a, b;
+
+
+        ///Form Load ////
+
+        private void KeyLogForm_Load(object sender, EventArgs e)
+        {
+            a = 0;
+            b = 0;
+        }
+
+
+        //// Timers ////
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lvEventLog = new ListViewItem(Cursor.Position.X.ToString());
+            lvEventLog.SubItems.Add(Cursor.Position.Y.ToString());
+            listView1.Items.Add(lvEventLog);
+            b++;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (a != b)
+            {
+                Cursor.Position = new Point(int.Parse(listView1.Items[a].SubItems[0].Text), int.Parse(listView1.Items[a].SubItems[1].Text));
+                a++;
+            }
+        }
+
+
+        //// Buttons ////
+
+        private void btnRec_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            timer2.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+        }
+    }
+}
